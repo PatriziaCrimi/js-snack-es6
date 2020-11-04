d4cdad1c87ca3c0cbe4a00cb0395483cb5da1595
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-11-04 16:50:02 +0100

    JSnack 2 - Solution 1: forEach

--- js-jsnack2/action.js
+++ js-jsnack2/action.js
@@ -9,7 +9,16 @@ Utilizzare nuova sintassi di JS ES6.
 $(document).ready(function () {
   // Creating an array of names
   let names_list = ['William', 'Robert', 'Anne', 'Christian', 'Josephine', 'Karleen', 'Astrid', 'Clark', 'Judith'];
+  // Printing in console
   console.log('The array containing the names is: ', names_list)
+  /*
+  // Template literal --> it converts my array into a string
+  console.log(
+  `
+  The array containing the names is: ${names_list}
+  `
+  );
+  */
 
   // Entering first number
   let first_num = parseInt(prompt('Please enter the first number.'));
@@ -24,44 +33,40 @@ $(document).ready(function () {
     second_num = parseInt(prompt('ERROR. You cannot enter two equal figures. Please enter a number ranging from 1 to ' + names_list.length + ' and different from ' + first_num + '.'));
     second_num = validNumber(second_num, names_list.length);
   }
-  console.log('The two numbers entered by the user are:', first_num, 'and', second_num, '.');
+  // Print in console --> it converts numbers into strings
+  console.log(
+  `
+  The two numbers entered by the user are: ${first_num} and ${second_num}.
+  `
+  );
 
   // Checking highest and lowest number
   if (first_num > second_num) {
-    let highest_num = first_num;
-    let lowest_num = second_num -1;
+    var highest_num = first_num;
+    var lowest_num = second_num -1;
   } else {
-    let highest_num = second_num;
-    let lowest_num = first_num -1;
+    var highest_num = second_num;
+    var lowest_num = first_num -1;
   }
 
   // Creating the new array containing the names included in the user's numbers range
   let names_final_array = [];
 
-  // ******************* SOLUTION 1 *******************
-  // Scanning only part of the names array (only what is included in the two numbers given by the user)
+  // ******************* SOLUTION 1 - FOREACH *******************
+
+  // Scanning the whole array of names (with forEach)
   names_list.forEach((item, i) => {
-    
-  });
-
-
-
-  for (let i = lowest_num; i < highest_num; i++) {
-    names_final_array.push(names_list[i]);
-  }
-  /*
-  // ******************* SOLUTION 2 *******************
-  // Scanning the whole array of names
-  for (i = 0; i < names_list.length; i++) {
     if (i >= lowest_num && i < highest_num) {
       names_final_array.push(names_list[i]);
     }
-  }
-  */
+  });
+
   /*
-  // ******************* SOLUTION 3 - SLICE *******************
-  names_final_array = names_list.slice(lowest_num, highest_num);
+  // ******************* SOLUTION 2 - FILTER *******************
+  // TO BE DONE
   */
+
+  // Print in console
   console.log('The new array containing the names included in the user\'s numbers range is: ', names_final_array);
 });
 
